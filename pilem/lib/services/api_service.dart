c24b216056c7bc42b015bbaff146a65ad1071a8b
Kevin <kevinfernandocahyadi_2226240053@mhs.mdp.ac.id>
2025-03-05 17:11:39 +0700

    update pilem searchmovie

--- pilem/lib/services/api_service.dart
+++ pilem/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey = '0040c3c99d3c83daa8d32ae144ee192e';
+  static const String apiKey = '2af55135711a91f3b077e3e21d665b9e';
 
   Future <List<Map<String, dynamic>>> getAllMovies() async {
     final response = await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
@@ -18,6 +18,11 @@ class ApiService {
   }
   Future <List<Map<String, dynamic>>> getPopularMovies() async {
     final response = await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+  Future<List<Map<String, dynamic>>> searchMovies(String query) async {
+    final response = await http.get(Uri.parse("$baseUrl/search/movie?query=$query&api_key=$apiKey"));
     final data = json.decode(response.body);
     return List<Map<String, dynamic>>.from(data['results']);
   }
